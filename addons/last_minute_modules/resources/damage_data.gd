745c445769f42b287337cf750eba80dc1cb1b699
Pink-Mario <96379560+Pink-Mario@users.noreply.github.com>
2026-02-17 11:40:13 -0500

    Adding DamageData resource
    Also included GroupModule inside of _enable_plugin function.

---
+++ addons/last_minute_modules/resources/damage_data.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name DamageData
+
+@export var dmg: int = 10
+@export var dmg_type: String = "None"
+
+@export var has_knockback: bool = true
+@export var knockback_power: float = 100.0
+@export var knockback_duration: float = 0.25
