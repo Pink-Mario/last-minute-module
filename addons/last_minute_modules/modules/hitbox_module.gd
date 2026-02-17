bc9948ad4540c702bd1e8624e4be0f99c43d6ef8
Pink-Mario <96379560+Pink-Mario@users.noreply.github.com>
2026-02-17 12:09:34 -0500

    Added functionality for hit/hurt boxes

--- addons/last_minute_modules/modules/hitbox_module.gd
+++ addons/last_minute_modules/modules/hitbox_module.gd
@@ -3,3 +3,31 @@ class_name HitboxModule
 
 @export var damage_data: DamageData
 @export var group_module: GroupModule
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	if damage_data:
+		damage_data = damage_data.duplicate()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is HurtboxModule:
+		var hurtbox = area as HurtboxModule
+		var hurtbox_health = hurtbox.health_module as HealthModule
+		
+		if not area.is_same_team(self):
+			hurtbox.take_hit(self)
+
+func get_damage():
+	return damage_data.dmg
+
+func set_damage(new_damage: int) -> void:
+	if damage_data:
+		damage_data.dmg = new_damage
+
+func modify_damage(amount: int) -> void:
+	if damage_data:
+		damage_data.dmg += amount
+
+func set_knockback_power(power: float) -> void:
+	if damage_data:
+		damage_data.knockback_power = power
--- addons/last_minute_modules/modules/hurtbox_module.gd
+++ addons/last_minute_modules/modules/hurtbox_module.gd
@@ -1,5 +1,19 @@
 extends Area2D
 class_name HurtboxModule
 
+@export var velocity_module: VelocityModule
 @export var health_module: HealthModule
 @export var group_module: GroupModule
+
+func take_hit(hitbox: HitboxModule):
+	if health_module and hitbox.damage_data:
+		health_module.take_damage(hitbox.damage_data.dmg, hitbox)
+		if velocity_module and hitbox.damage_data.has_knockback:
+			var knockback_dir = hitbox.global_position.direction_to(global_position)
+			var knockback_force = knockback_dir * hitbox.damage_data.knockback_power
+			velocity_module.apply_knockback(knockback_force, hitbox.damage_data.knockback_duration)
+
+func is_same_team(hitbox: HitboxModule) -> bool:
+	if group_module == null or hitbox.group_module == null:
+		return true
+	return group_module.group != hitbox.group_module.group
