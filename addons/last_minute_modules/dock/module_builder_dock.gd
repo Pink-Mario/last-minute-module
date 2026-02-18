e2185df2b6f260954eb15d97364cd0035958f722
Pink-Mario <96379560+Pink-Mario@users.noreply.github.com>
2026-02-18 08:53:20 -0500

    Retooled Module Builder
    Now only allows for quickly adding nodes into the current scene

--- addons/last_minute_modules/dock/module_builder_dock.gd
+++ addons/last_minute_modules/dock/module_builder_dock.gd
@@ -1,50 +1,8 @@
 @tool
-extends VBoxContainer
+extends ScrollContainer
 
 var editor_plugin: EditorPlugin
-
-var templates := {
-	"Sidescroller Character": {
-		"base": "CharacterBody2D",
-		"modules": ["SidescrollerVelocityModule", "HealthModule", "HurtboxModule", "GroupModule", "StateHandler"],
-		"children": [
-			{"type": "CollisionShape2D", "name": "CollideShape"},
-			{"type": "Sprite2D", "name": "PlayerSprite"},
-		]
-	},
-	"Topdown Character": {
-		"base": "CharacterBody2D",
-		"modules": ["TopdownVelocityModule", "HealthModule", "HurtboxModule", "GroupModule", "StateHandler"],
-		"children": [
-			{"type": "CollisionShape2D", "name": "CollideShape"},
-			{"type": "Sprite2D", "name": "PlayerSprite"},
-		]
-	},
-	"Sidescroller Enemy": {
-		"base": "CharacterBody2D",
-		"modules": ["SidescrollerVelocityModule", "PathfindModuleSideScroller", "HealthModule", "HurtboxModule", "HitboxModule", "GroupModule", "StateHandler"],
-		"children": [
-			{"type": "CollisionShape2D", "name": "CollideShape"},
-			{"type": "Sprite2D", "name": "EnemySprite"},
-		]
-	},
-	"Topdown Enemy": {
-		"base": "CharacterBody2D",
-		"modules": ["TopdownVelocityModule", "PathfindModuleTopDown", "HealthModule", "HurtboxModule", "HitboxModule", "GroupModule", "StateHandler"],
-		"children": [
-			{"type": "CollisionShape2D", "name": "CollideShape"},
-			{"type": "Sprite2D", "name": "EnemySprite"},
-		]
-	},
-	"Projectile": {
-		"base": "ProjectileModule",
-		"modules": ["GroupModule"],
-		"children": [
-			{"type": "Sprite2D", "name": "ProjectileSprite"},
-			{"type": "CollisionShape2D", "name": "CollideShape"},
-		]
-	},
-}
+@onready var module_builder_dock_v: VBoxContainer = $ModuleBuilderDockV
 
 var quick_modules := [
 	"SidescrollerVelocityModule",
@@ -60,30 +18,38 @@ var quick_modules := [
 	"MuzzleModule",
 ]
 
+var module_paths := {
+	"SidescrollerVelocityModule": "res://addons/last_minute_modules/modules/velocity_sidescroll_module.gd",
+	"TopdownVelocityModule": "res://addons/last_minute_modules/modules/velocity_module_topdown.gd",
+	"PathfindModuleSideScroller": "res://addons/last_minute_modules/modules/pathfind_module_sidescroller.gd",
+	"PathfindModuleTopDown": "res://addons/last_minute_modules/modules/pathfind_module_topdown.gd",
+	"HealthModule": "res://addons/last_minute_modules/modules/health_module.gd",
+	"IFrameModule": "res://addons/last_minute_modules/modules/iframe_module.gd",
+	"HurtboxModule": "res://addons/last_minute_modules/modules/hurtbox_module.gd",
+	"HitboxModule": "res://addons/last_minute_modules/modules/hitbox_module.gd",
+	"StateHandler": "res://addons/last_minute_modules/modules/state_handler/state_handler.gd",
+	"State": "res://addons/last_minute_modules/modules/state_handler/state.gd",
+	"MuzzleModule": "res://addons/last_minute_modules/modules/muzzle_module.gd",
+	"GroupModule": "res://addons/last_minute_modules/modules/group_module.gd",
+	"ProjectileModule": "res://addons/last_minute_modules/modules/projectile_module.gd",
+}
+
 func _ready() -> void:
 	var template_label := Label.new()
-	template_label.text = "Character Templates"
+	template_label.text = "Tree Templates"
 	template_label.add_theme_font_size_override("font_size", 14)
-	add_child(template_label)
+	module_builder_dock_v.add_child(template_label)
 	
 	var template_container := VBoxContainer.new()
-	add_child(template_container)
-	
-	for template_name in templates.keys():
-		var btn := Button.new()
-		btn.text = template_name
-		btn.pressed.connect(_on_template_pressed.bind(template_name))
-		template_container.add_child(btn)
-	
-	add_child(HSeparator.new())
+	module_builder_dock_v.add_child(template_container)
 	
 	var module_label := Label.new()
-	module_label.text = "Add Module to Selected"
+	module_label.text = "Quick Modules"
 	module_label.add_theme_font_size_override("font_size", 14)
-	add_child(module_label)
+	module_builder_dock_v.add_child(module_label)
 	
 	var module_container := VBoxContainer.new()
-	add_child(module_container)
+	module_builder_dock_v.add_child(module_container)
 	
 	for module_name in quick_modules:
 		var btn := Button.new()
@@ -91,67 +57,42 @@ func _ready() -> void:
 		btn.pressed.connect(_on_module_pressed.bind(module_name))
 		module_container.add_child(btn)
 	
-	add_child(HSeparator.new())
+	module_builder_dock_v.add_child(HSeparator.new())
 	
 	var helper_label := Label.new()
-	helper_label.text = "Quick Helpers"
+	helper_label.text = "Convenice"
 	helper_label.add_theme_font_size_override("font_size", 14)
-	add_child(helper_label)
+	module_builder_dock_v.add_child(helper_label)
 	
 	var helper_container := VBoxContainer.new()
-	add_child(helper_container)
+	module_builder_dock_v.add_child(helper_container)
 	
 	var sprite_btn := Button.new()
-	sprite_btn.text = "+ Sprite2D"
+	sprite_btn.text = "+ Sprite"
 	sprite_btn.pressed.connect(_add_base_node.bind("Sprite2D"))
 	helper_container.add_child(sprite_btn)
 	
 	var collision_btn := Button.new()
-	collision_btn.text = "+ CollisionShape2D"
+	collision_btn.text = "+ Collision Shape"
 	collision_btn.pressed.connect(_add_base_node.bind("CollisionShape2D"))
 	helper_container.add_child(collision_btn)
 	
 	var marker_btn := Button.new()
-	marker_btn.text = "+ Marker2D"
-	marker_btn.pressed.connect(_add_base_node.bind("Marker2D"))
+	marker_btn.text = "+ Animation"
+	marker_btn.pressed.connect(_add_base_node.bind("AnimationPlayer"))
 	helper_container.add_child(marker_btn)
 
-func _on_template_pressed(template_name: String) -> void:
-	var template = templates[template_name]
-	var editor_interface := editor_plugin.get_editor_interface()
-	var edited_scene := editor_interface.get_edited_scene_root()
-	
-	if not edited_scene:
-		push_error("No scene is currently open")
-		return
-	
-	var base_node: Node = ClassDB.instantiate(template.base)
-	base_node.name = template_name.replace(" ", "")
-
-	for module_name in template.modules:
-		var module: Node = ClassDB.instantiate(module_name)
-		base_node.add_child(module)
-		module.owner = edited_scene
-
-	for child_data in template.children:
-		var child: Node = ClassDB.instantiate(child_data.type)
-		child.name = child_data.name
-
-		if child_data.has("parent"):
-			var parent = base_node.get_node(child_data.parent)
-			if parent:
-				parent.add_child(child)
-				child.owner = edited_scene
-		else:
-			base_node.add_child(child)
-			child.owner = edited_scene
-	
-	edited_scene.add_child(base_node)
-	base_node.owner = edited_scene
-	
-	editor_interface.get_selection().clear()
-	editor_interface.get_selection().add_node(base_node)
-
+func _create_node(node_type: String) -> Node:
+	var node
+	if module_paths.has(node_type):
+		var script = load(module_paths[node_type])
+		node = script.new()
+		return node
+	else:
+		node = ClassDB.instantiate(node_type)
+	node.name = node_type
+	return node
+		
 func _on_module_pressed(module_name: String) -> void:
 	var editor_interface := editor_plugin.get_editor_interface()
 	var selection := editor_interface.get_selection()
@@ -164,11 +105,10 @@ func _on_module_pressed(module_name: String) -> void:
 	var target_node = selected_nodes[0]
 	var edited_scene := editor_interface.get_edited_scene_root()
 
-	var module: Node = ClassDB.instantiate(module_name)
+	var module: Node = _create_node(module_name)
 	target_node.add_child(module)
 	module.owner = edited_scene
 	
-	# If it's a hitbox/hurtbox/projectile, add a CollisionShape2D child
 	if module_name in ["HitboxModule", "HurtboxModule", "ProjectileModule"]:
 		var collision := CollisionShape2D.new()
 		collision.name = "CollisionShape2D"
@@ -187,6 +127,6 @@ func _add_base_node(node_type: String) -> void:
 	var target_node = selected_nodes[0]
 	var edited_scene := editor_interface.get_edited_scene_root()
 	
-	var new_node: Node = ClassDB.instantiate(node_type)
+	var new_node: Node = _create_node(node_type)
 	target_node.add_child(new_node)
 	new_node.owner = edited_scene
